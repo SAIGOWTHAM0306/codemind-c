7a2ffab1d2562b9fd197b261754e87a52a187466
SAIGOWTHAM0306 <119730087+SAIGOWTHAM0306@users.noreply.github.com>
2023-05-23 19:34:56 +0530

    #include<stdio.h> int main() {     int n,c;     scanf("%d",&n);     int a=0,b=1;     for(int i=1;i<=n;i++)     {
            c=a+b;
            if(a==n)
            {
                printf("True");
                return 0;
            }
            a=b;
            b=c;
        }
        printf("False");
    }

--- Fibonacci_or_not.c
+++ Fibonacci_or_not.c
@@ -1,49 +1,20 @@
-#include <stdio.h>
-int main() {
-
-  int i, n;
-
-  // initialize first and second terms
-  int t1 = 0, t2 = 1;
-
-  // initialize the next term (3rd term)
-  int nextTerm = t1 + t2;
-
-  // get no. of terms from user
-  scanf("%d", &n);
-
-  // print the first two terms t1 and t2
-  
-
-  // print 3rd to nth terms
-  while(nextTerm<n) {
-    
-    t1 = t2;
-    t2 = nextTerm;
-    nextTerm = t1 + t2;
-  }
-  if(nextTerm==n)
-  {
-      printf("True");
-  }
-  else
-  {
-      printf("False");
-  }
-
-  return 0;
+#include<stdio.h>
+int main()
+{
+    int n,c;
+    scanf("%d",&n);
+    int a=0,b=1;
+    for(int i=1;i<=n;i++)
+    {
+        
+        c=a+b;
+        if(a==n)
+        {
+            printf("True");
+            return 0;
+        }
+        a=b;
+        b=c;
+    }
+    printf("False");
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
