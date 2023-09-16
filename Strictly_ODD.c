80263cf211cfdecd5249020713e4b2e40665d229
SAIGOWTHAM0306 <119730087+SAIGOWTHAM0306@users.noreply.github.com>
2023-09-16 10:29:01 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int a[n],i,c=0,s=0;     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         if(a[i]%2==1)         {             c++;         }     }     for(i=0;i<n;i++)     {         if(a[i]%2==1 && i%2==1)         {             s++;         }     }     if(s==c)     printf("True");     else     printf("False"); }

--- Strictly_ODD.c
+++ Strictly_ODD.c
@@ -1,34 +1,29 @@
 #include<stdio.h>
 int main()
 {
-	int n,i,a[n],c;
-	scanf("%d",&n);
-	for(i=0;i<n;i++)
-	{
-		scanf("%d",&a[i]);
-	}	
-		for(i=0;i<n;i++) 
-		{
-          if(a[i]%2!=0)
-          {
-              if(i%2!=0)
-              {
-                  c=0;
-              }
-              else
-              {
-                  c=1;
-                  break;
-              }
-          }
-		}
-		if(c!=1)
-		{
-		    printf("True");
-		}
-		else
-		{
-		    printf("False");
-		}
-		
+    int n;
+    scanf("%d",&n);
+    int a[n],i,c=0,s=0;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]%2==1)
+        {
+            c++;
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]%2==1 && i%2==1)
+        {
+            s++;
+        }
+    }
+    if(s==c)
+    printf("True");
+    else
+    printf("False");
 }
